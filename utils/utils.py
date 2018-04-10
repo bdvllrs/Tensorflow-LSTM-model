d946b59de22f46cb2a6bfda6fbcbe1dd6665e673
Benjamin Devillers <bdvllrs@gmail.com>
2018-04-10 22:33:58 +0200

    add index_to_word_transform

--- utils/utils.py
+++ utils/utils.py
@@ -11,3 +11,15 @@ def word_to_index_transform(word_to_index, data):
         assert word in word_to_index.keys(), "The word {} is not in the vocab".format(word)
         return word_to_index[word]
     return np.array(list(map(lambda sequence: list(map(transform_word, sequence)), data)))
+
+
+def index_to_word_transform(index_to_word, data):
+    """
+    Get a batch of sequences and transform the words into indices
+    :param index_to_word: dict of relations between words and indices
+    :param data: batch to apply the transformation on.
+    """
+    def transform_word(index):
+        assert index in index_to_word.keys(), "The index {} is not in the vocab".format(index)
+        return index_to_word[index]
+    return np.array(list(map(lambda sequence: list(map(transform_word, sequence)), data)))
